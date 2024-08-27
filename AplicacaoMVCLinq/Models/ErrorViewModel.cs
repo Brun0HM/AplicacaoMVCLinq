b3155eae9bdd36f38c83eaecbd0811afbe3c48fa
Brun0HM <brunodev41@gmail.com>
2024-08-27 14:55:59 -0300

    Adicionar arquivos de projeto.

---
+++ AplicacaoMVCLinq/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AplicacaoMVCLinq.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
